c8b4930be86f3e83be7c556a80244f369f83eabf
Pavel <pavelpredator6@gmail.com>
2024-02-27 21:49:32 +0300

    Add base URL for GitHub Pages deployment

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -9,7 +9,7 @@ const NotFound: React.FC = () => {
       <h1 className='text-4xl font-bold mb-4'>404. Oops!</h1>
       <p className='text-lg text-gray-600'>
         Sorry, an unexpected error has occurred.
-        <i>{(error as Error).statusText || (error as Error).message}</i>
+        <i>{(error as Error).message}</i>
       </p>
       <Link to='/' className='text-blue-600 underline mt-4'>
         Go back to home
